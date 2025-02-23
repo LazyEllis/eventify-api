e57a269c79a9e8d9f1928584524dc960b662dc06
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-02-23 16:58:11 +0100

    Fix bug where error messages weren't sent in JSON

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use("/", virtualEventRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(err.statusCode || 500).send(err.message);
+  res.status(err.statusCode || 500).send({ message: err.message });
   next();
 });
 
